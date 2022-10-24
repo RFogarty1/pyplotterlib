73d1540ae59e2838a1c700d3c8a299cb2b2f47e1
Richard Fogarty <richard.m.fogarty@gmail.com>
2022-10-24 15:46:17 +0100

    plotters/shared: Added a fromPlotter Mixin option
    This allows a plotter to be initialized using another plotter; intended use is when reading plotters written in earlier versions

--- src/pyplotterlib/standard/private/shared.py
+++ src/pyplotterlib/standard/private/shared.py
@@ -17,6 +17,38 @@ class FromJsonMixin():
 		return outObj
 
 
+class FromPlotterMixin():
+
+	@classmethod
+	def fromPlotter(cls, inpPlotter):
+		""" Create the plotter using options from inpPlotter.
+		
+		Args:
+			inpPlotter: (PlotterInter) Should generally be the same type as the target, though doesnt have to be (see Notes for behaviour details).
+		
+		Returns:
+			outPlotter: (PlotterInter)
+
+
+		Notes:
+			a) We dont explicitly copy anything by default. So a SimpleNamespace option-value on outPlotter will point to the same object as on inpPlotter. You may wish to use copy.deepcopy(outPlotter) for safety.
+			b) outPlotter will set every relevant option it finds on inpPlotter. If an option is ONLY on inpPlotter, it will be ignored. If an option is ONLY on outPlotter, the default value will be used.
+			c) w.r.t. point b); note that some options with the same name (e.g. "plotData") may mean different things on different plotters. Thus, care should be exercised if type(inpPlotter) does not match type(outPlotter)
+
+		 
+		"""
+		outPlotter = cls()
+		sharedOptNames = set(inpPlotter.optionNames).intersection(outPlotter.optionNames)
+
+		optDict = dict()
+		for optName in sharedOptNames:
+			currVal = getattr(inpPlotter.opts,optName).value
+			optDict[optName] = currVal
+
+		outPlotter.setOptionVals(optDict)
+
+		return outPlotter
+
 
 def _getAxisEdges(inpAx):
 	startX, startY, xLength, yLength = inpAx.get_position().bounds
